4f66b62ce6e1d665183991f2ca60e50d756b95e8
gallegodev <gallegodev1991@gmail.com>
2022-12-11 16:05:15 +0000

    Update color schemes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-let playerText =document.getElementById('playerText');
 let restartBtn = document.getElementById('restartBtn');
-let boxes = Array.from (document.getElementsByClassName('box'))
+let boxes = Array.from(document.getElementsByClassName('box'))
 
 const O_TEXT = "O";
 const X_TEXT = "X";
+
 let currentPlayer = X_TEXT;
 let spaces = Array(9).fill(null);
 let count_plays = 0
@@ -15,41 +15,43 @@ const startGame = () => {
 function boxClicked(e) {
     const id = e.target.id
 
-    if(!spaces[id] && count_plays < 9){
+    if (!spaces[id] && count_plays < 9) {
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
 
-        if(playerHasWon() !==false){
+        if (playerHasWon() !== false) {
             var changeFrase = document.getElementById("mainFrase");
             changeFrase.innerHTML = `Player ${currentPlayer} won!`;
             let winning_blocks = playerHasWon()
             count_plays = 10
+        } 
+
         }
-        
+
         count_plays++
         currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
     }
-}
+
+
 
 const winningCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
-function playerHasWon(){
+function playerHasWon() {
     for (const condition of winningCombos) {
-        let [a,b,c] = condition
+        let [a, b, c] = condition
 
-        if(spaces [a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
-            return [a,b,c]
-        }
-        
+        if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a, b, c]
+        } 
     }
     return false
 }
@@ -61,12 +63,12 @@ function restart() {
     changeFrase.innerHTML = "The God Anubis is challenging you mortal!";
     spaces.fill(null)
     count_plays = 0
-    boxes.forEach( box => {
+    boxes.forEach(box => {
         box.innerText = ''
-        
+
     })
 
-    
+
 
     currentPlayer = X_TEXT
 }
